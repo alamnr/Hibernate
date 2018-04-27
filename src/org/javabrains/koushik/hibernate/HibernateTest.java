d74c3531fe0e8d3fda576e3fbf951ec7f5217746
alamnr <alamnr@gmail.com>
2018-04-27 15:24:12 +0600

    setting hbm2ddl.auto config and entity table and column name annotation

--- src/org/javabrains/koushik/hibernate/HibernateTest.java
+++ src/org/javabrains/koushik/hibernate/HibernateTest.java
@@ -11,8 +11,8 @@ public class HibernateTest {
 	public static void main(String[] args)
 	{
 		UserDetails user = new UserDetails();
-		user.setUserId(1);
-		user.setUserName("First User");
+		user.setUserId(3);
+		user.setUserName("Third User");
 		
 		//SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		SessionFactory sessionFactory = new AnnotationConfiguration().configure("hibernate.cfg.xml").buildSessionFactory();
