e0d507985bf5e2ae6e60cc31d5c50e014e4d10de
alamnr <alamnr@gmail.com>
2018-05-04 20:10:42 +0600

    Commit for Implementing Inheritance-Single Table

--- src/org/javabrains/koushik/dto/Vehicle.java
+++ src/org/javabrains/koushik/dto/Vehicle.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
@@ -17,6 +21,11 @@ import org.hibernate.annotations.NotFoundAction;
 
 // Entity Object, requires mapping in hibernate.cfg.xml
 @Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(
+ name="VEHICLE_TYPE",
+ discriminatorType= DiscriminatorType.STRING
+)
 public class Vehicle {
 	
 	@Id	@GeneratedValue(strategy=GenerationType.AUTO)
