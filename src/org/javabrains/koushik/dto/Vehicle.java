3c3c399117b6638a258bc225e894c5d1f689eaad
alamnr <alamnr@gmail.com>
2018-05-04 23:09:01 +0600

    commit done with exception: org.hibernate.MappingException: Cannot use identity column key generation with <union-subclass> mapping for: org.javabrains.koushik.dto.FourWheeler

--- src/org/javabrains/koushik/dto/Vehicle.java
+++ src/org/javabrains/koushik/dto/Vehicle.java
@@ -21,11 +21,7 @@ import org.hibernate.annotations.NotFoundAction;
 
 // Entity Object, requires mapping in hibernate.cfg.xml
 @Entity
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(
- name="VEHICLE_TYPE",
- discriminatorType= DiscriminatorType.STRING
-)
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 public class Vehicle {
 	
 	@Id	@GeneratedValue(strategy=GenerationType.AUTO)
