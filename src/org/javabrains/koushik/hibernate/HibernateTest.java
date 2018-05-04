21a2228a40dc856bc10817d2d2d1044f5c23d41b
alamnr <alamnr@gmail.com>
2018-05-04 19:51:47 +0600

    Commit for Implementing Inheritance

--- src/org/javabrains/koushik/hibernate/HibernateTest.java
+++ src/org/javabrains/koushik/hibernate/HibernateTest.java
@@ -19,33 +19,39 @@ import org.hibernate.in.action.book.selfAssociationAndValueType.dto.Category;
 import org.hibernate.in.action.book.selfAssociationAndValueType.dto.Message;
 import org.hibernate.in.action.book.util.HibernateUtil;
 import org.javabrains.koushik.dto.Address;
+import org.javabrains.koushik.dto.FourWheeler;
+import org.javabrains.koushik.dto.TwoWheeler;
 import org.javabrains.koushik.dto.UserDetails;
 import org.javabrains.koushik.dto.Vehicle;
 
 public class HibernateTest {
 
 	public static void main(String[] args) {
+
+		/*
+		 * UserDetails user = new UserDetails(); user.setUserName("First User");
+		 */
+		Vehicle vehicle = new Vehicle();
+		vehicle.setVehicleName("Car");
+
+		TwoWheeler bike = new TwoWheeler();
+		bike.setVehicleName("Bike");
+		bike.setSteeringHandle("Bike Steering Handle");
 		
-		
-		   UserDetails user = new UserDetails();
-		   user.setUserName("First User");
-		   
-		   Vehicle vehicle = new Vehicle(); 
-		   vehicle.setVehicleName("Car");
-		   
-		   Vehicle vehicle2 = new Vehicle(); 
-		   vehicle2.setVehicleName("Zeep");
-		  
-		   user.getVehicleList().add(vehicle);
-		   user.getVehicleList().add(vehicle2);
-		   
-		  // vehicle.setUser(user);
-		   //vehicle2.setUser(user);
-		  
-		   /*vehicle.getUsers().add(user); 
-		   vehicle2.getUsers().add(user);*/
-		 
-		
+		FourWheeler car = new FourWheeler();
+		car.setVehicleName("Porsche");
+		car.setSteeringWheel("Porsche Steering Wheel");
+
+		// user.getVehicleList().add(vehicle);
+		// user.getVehicleList().add(vehicle2);
+
+		// vehicle.setUser(user);
+		// vehicle2.setUser(user);
+
+		/*
+		 * vehicle.getUsers().add(user); vehicle2.getUsers().add(user);
+		 */
+
 		// Bidirectional Association (OneToMany and ManyToOne)
 
 		/*
@@ -54,11 +60,11 @@ public class HibernateTest {
 		 * message.setNextMessage(newMessage);
 		 */
 
-		Category aParent = new Category();
+		/*Category aParent = new Category();
 		aParent.setName("Parent");
 		Category aChild = new Category();
 		aChild.setName("Child");
-		aParent.addChildCategory(aChild);
+		aParent.addChildCategory(aChild);*/
 
 		/*
 		 * // Table per concrete class with implicit polymorphism CreditCard cc
@@ -68,44 +74,36 @@ public class HibernateTest {
 		 * DebitCard dc = new DebitCard(); dc.setOwner("Rin");
 		 * dc.setNumber("444-555-666");
 		 */
-/*
-		// Table per concrete class with implicit polymorphism
-		CreditCard_1 cc = new CreditCard_1();
-		cc.setOwner("Khelapi");
-		cc.setNumber("111-222-333");
-
-		DebitCard_1 dc = new DebitCard_1();
-		dc.setOwner("Rin");
-		dc.setNumber("444-555-666");
-*/
-		
+		/*
+		 * // Table per concrete class with implicit polymorphism CreditCard_1
+		 * cc = new CreditCard_1(); cc.setOwner("Khelapi");
+		 * cc.setNumber("111-222-333");
+		 * 
+		 * DebitCard_1 dc = new DebitCard_1(); dc.setOwner("Rin");
+		 * dc.setNumber("444-555-666");
+		 */
 
 		// Single Table
-		/* 
-		
-		CreditCard_2 cc = new CreditCard_2();
-		cc.setOwner("Khelapi");
-		cc.setNumber("111-222-333");
-
-		DebitCard_2 dc = new DebitCard_2();
-		dc.setOwner("Rin");
-		dc.setNumber("444-555-666");
-*/
+		/*
+		 * 
+		 * CreditCard_2 cc = new CreditCard_2(); cc.setOwner("Khelapi");
+		 * cc.setNumber("111-222-333");
+		 * 
+		 * DebitCard_2 dc = new DebitCard_2(); dc.setOwner("Rin");
+		 * dc.setNumber("444-555-666");
+		 */
 		// Bidirectional Association (OneToMany and ManyToOne)
-		
-		/*Bid bid = new Bid();
-		bid.setAmount(30);
 
-		Bid bid_2 = new Bid();
-		bid_2.setAmount(130);
-		
-		Item item = new Item();
-		
-		item.setItemName("Laptop");
-		item.addBid(bid);
-		item.addBid(bid_2);*/
-		
-		
+		/*
+		 * Bid bid = new Bid(); bid.setAmount(30);
+		 * 
+		 * Bid bid_2 = new Bid(); bid_2.setAmount(130);
+		 * 
+		 * Item item = new Item();
+		 * 
+		 * item.setItemName("Laptop"); item.addBid(bid); item.addBid(bid_2);
+		 */
+
 		/*
 		 * SessionFactory sessionFactory = new
 		 * AnnotationConfiguration().configure("hibernate.cfg.xml")
@@ -115,45 +113,49 @@ public class HibernateTest {
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 
-	/*	
-		session.save(item);
-		session.save(bid);
-		session.save(bid_2);*/
-		
-		//session.save(cc);
-		//session.save(dc);
+		/*
+		 * session.save(item); session.save(bid); session.save(bid_2);
+		 */
+
+		// session.save(cc);
+		// session.save(dc);
 
 		// session.save(aChild);
 		// session.save(aParent);
 		// session.save(message);
-		 //session.save(user);
-		session.persist(user);
-		 /*session.save(vehicle);
-		 session.save(vehicle2);*/
+		// session.save(user);
+		//session.persist(user);
+		/*
+		 * session.save(vehicle); session.save(vehicle2);
+		 */
+		
+		session.save(vehicle);
+		session.save(bike);
+		session.save(car);
 		session.getTransaction().commit();
 		session.close();
-		
-		//item = null;
-		
-		user = null;
-		vehicle2 =null;
-		vehicle =null;
-		session = sessionFactory.openSession();
-		//item = (Item) session.get(Item.class, 1L);*/
-		user = (UserDetails) session.get(UserDetails.class, 1);
-		vehicle2 = (Vehicle) session.get(Vehicle.class, 2);
-		vehicle = (Vehicle) session.get(Vehicle.class, 1);
-		session.close();
-		
-		//System.out.println("Bid count:"+item.getBids().size());
-		
-		//System.out.println("Vehicle count:"+user.getVehicleList().size());
-		
-		/*System.out.println("User Name: " +  vehicle.getUser().getUserName());
-		
-		System.out.println("User Name: " +  vehicle2.getUser().getUserName());
-		*/
-		
+
+		// item = null;
+
+		//user = null;
+		//vehicle2 = null;
+		//vehicle = null;
+		//session = sessionFactory.openSession();
+		// item = (Item) session.get(Item.class, 1L);
+		//user = (UserDetails) session.get(UserDetails.class, 1);
+		//vehicle2 = (Vehicle) session.get(Vehicle.class, 2);
+		//vehicle = (Vehicle) session.get(Vehicle.class, 1);
+		//session.close();
+
+		// System.out.println("Bid count:"+item.getBids().size());
+
+		// System.out.println("Vehicle count:"+user.getVehicleList().size());
+
+		/*
+		 * System.out.println("User Name: " + vehicle.getUser().getUserName());
+		 * 
+		 * System.out.println("User Name: " + vehicle2.getUser().getUserName());
+		 */
 
 		HibernateUtil.shutDown();
 
